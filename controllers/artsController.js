6ee64aba5637b3cd182ff2fd6047851271af5df4
Natalia Polishchuk <Natalia@Natalias-MacBook-Pro.local>
2016-10-04 15:11:24 -0700

    npm install

--- controllers/artsController.js
+++ controllers/artsController.js
@@ -3,25 +3,7 @@
  ************/
 
 /* hard-coded data */
-var arts =[];
-artList.push({
-              _id:1;
-              image: '/images/1.jpg',
-              info: 'A 1/3 scale replica of an American Frigate from the late 18th century.',
-              year: '2016',
-            });
-artList.push({
-            _id:2;
-            image: '/images/2.jpg',
-            info: 'Wreckage International creation - The Lost Tea Party.',
-            year: '2016',
-          });
-artList.push({
-              _id:3;
-              image: '/images/3.jpg',
-              info: 'A tribute to Dali.',
-              year: '2016',
-            });
+
 
 
 
