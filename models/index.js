885ce93d4578a062293750f2d032aba4f81ade07
Natalia Polishchuk <Natalia@natalias-mbp.gateway.sonic.net>
2016-10-05 00:37:07 -0700

    update db connection to point to heroku db

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/art-of-playa");
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/art-of-playa");
+
 var Art = require('./art.js');
 module.exports.Art = Art;
