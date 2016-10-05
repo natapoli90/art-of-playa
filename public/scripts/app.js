2835b6ab9a8687fdb6c6b7fff554094f4d4c4f52
Natalia Polishchuk <Natalia@natalias-mbp.gateway.sonic.net>
2016-10-05 11:51:35 -0700

    add form and post method to vreate new art

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -13,20 +13,7 @@
    function ArtsIndexController ($http) {
      var vm = this;
      vm.newArt = {};
-     vm.arts = [
-        {
-          info: 'art',
-          year: '2016'
-        },
-        {
-          info: 'art',
-          year: '2016'
-        },
-        {
-          info: 'art',
-          year: '2016'
-        }
-      ];
+
 
      $http({
        method: 'GET',
@@ -36,6 +23,18 @@
      }, function errorCallback(response) {
        console.log('There was an error getting the data', response);
      });
+
+     vm.createArt = function () {
+         $http({
+           method: 'POST',
+           url: '/api/arts',
+           data: vm.newArt,
+         }).then(function successCallback(response) {
+           vm.arts.push(response.data);
+         }, function errorCallback(response) {
+           console.log('There was an error posting the data', response);
+         });
+       };
 
 
    }
